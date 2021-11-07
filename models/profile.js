0e533638e67f2add72629aac9ef8f703a23bb3c9
mohamadsaad1 <mohamad_utd@outlook.com>
2021-11-07 14:36:24 -0600

    anime and comment schema

--- models/profile.js
+++ models/profile.js
@@ -1,8 +1,26 @@
 import mongoose from 'mongoose'
 
-const profileSchema = new mongoose.Schema({
+const Schema = mongoose.Schema
+
+const commentSchema = new Schema({
+content: String,
+})
+
+
+const animeSchema = new Schema({
+  name: String,
+  releaseDate: String,
+  genre: String,
+  comments: [commentSchema],
+
+})
+
+
+const profileSchema = new Schema({
   name: String,
   avatar: String,
+  animeCompletedList: [animeSchema],
+  animeWatchedList: [animeSchema],
 }, {
   timestamps: true
 })
