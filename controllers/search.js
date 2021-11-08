10f1eba060d5bfa9166cd49858e204132a3056a0
mohamadsaad1 <mohamad_utd@outlook.com>
2021-11-07 23:07:41 -0600

    fix api

--- controllers/search.js
+++ controllers/search.js
@@ -1,13 +1,15 @@
 import axios from 'axios'
 
 function animeSearch(req, res) {
+  // axios.get(`https://api.jikan.moe/v3/anime/${req.body.query}`)
   axios.get(`https://api.jikan.moe/v3/search/anime?q=${req.body.query}`)
 
   .then(response => {
+    console.log(response.data.results)
     res.render('search',{
-      title: 'Search Results',
-      user: req.user,
-      anime: response.data
+      title: 'title',
+      anime: response.data.results,
+
     })
   })
   .catch(err => {
