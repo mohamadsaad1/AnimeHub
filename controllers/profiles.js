3a1de11b89589c028c509b29a2da14b080c4442d
mohamadsaad1 <mohamad_utd@outlook.com>
2021-11-08 09:58:48 -0600

    completed add to watch list function

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -1,8 +1,16 @@
 import { Profile } from "../models/profile.js"
 
-function addToWatchList(req, res){
-    
-
+function addToWatchList(req, res) {
+  // Find the profile
+  Profile.findById(req.user.profile._id)
+  .then(profile => {
+    // Push the anime data (req.body) into anime Schema
+    profile.animeWatchedList.push(req.body)
+    profile.save()
+    .then(()=> {
+      res.redirect('/')
+    })
+  })
 }
 
 export{
